b8cba0a8730dd5a3b4dc1025e33ea9e504f4c794
hammadsadi <hammad.sadi@yahoo.com>
2024-08-17 11:57:19 +0600

    Update Footer and Added Contact and About Page

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div className="text-center space-y-3 my-5">
+      <h2 className="text-2xl font-bold">About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+        officiis nostrum quas repudiandae fugit quisquam rerum mollitia non.
+        Accusamus quo et rerum eveniet beatae perspiciatis reprehenderit
+        architecto fuga illum distinctio Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Debitis, voluptate modi! Adipisci est quidem
+        repellendus beatae, harum odio molestiae blanditiis reiciendis vero a
+        nulla possimus rerum at fuga placeat. Illum officiis velit, aut
+        inventore possimus officia libero labore consectetur. Officia!
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,8 @@ import Layout from "../layout/Layout";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
+import Contact from "../pages/Contact/Contact";
+import About from "../pages/About/About";
 
 const routes = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const routes = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/about",
+        element: <About />,
       },
     ],
   },
